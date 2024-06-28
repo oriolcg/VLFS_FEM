e62693bd133f5acf4941a67a633801362c5213aa
Oriol Colomes <oriol.colomes@gmail.com>
2024-06-28 16:25:30 +0200

    Modifications for floating_ice (not working)

--- src/MonolithicFEMVLFS.jl
+++ src/MonolithicFEMVLFS.jl
@@ -19,6 +19,7 @@ include("Khabakhpasheva_time_domain.jl")
 include("Liu.jl")
 include("Yago_freq_domain.jl")
 include("Multi_geo_freq_domain.jl")
+include("floating_ice/Step.jl")
 
 using .Periodic_Beam: Periodic_Beam_params, run_periodic_beam
 using .Periodic_Beam_FS: Periodic_Beam_FS_params, run_periodic_beam_FS
@@ -27,6 +28,7 @@ using .Khabakhpasheva_time_domain: Khabakhpasheva_time_domain_params, run_Khabak
 using .Liu: Liu_params, run_Liu
 using .Yago_freq_domain: Yago_freq_domain_params, run_Yago_freq_domain
 using .MultiGeo_freq_domain: MultiGeo_freq_domain_params, run_MultiGeo_freq_domain
+using .Step: Step_params, run_Step
 
 # Extend DrWatson functions
 DrWatson.allaccess(c::Periodic_Beam_params) = (:n, :dt, :tf, :orderϕ, :orderη, :k)
@@ -54,6 +56,7 @@ include("../scripts/5-2-2-Khabakhpasheva-time-domain.jl")
 include("../scripts/5-3-1-Liu.jl")
 include("../scripts/5-4-1-Yago.jl")
 include("../scripts/5-5-1-MultiGeo.jl")
+include("../scripts/floating_ice/step_script.jl")
 
 function run_tests(test::String)
   if test=="all"
@@ -84,6 +87,10 @@ function run_tests(test::String)
     run_5_4_1_Yago()
   elseif test == "5-5-1" || test == "5-5-1-MultiGeo"
     run_5_5_1_MultiGeo()
+  elseif test == "step"
+    run_tmp_Step()
+  else
+    println("Test not found")
   end
 end
 
